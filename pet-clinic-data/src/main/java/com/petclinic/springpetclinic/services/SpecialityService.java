647a1a6b8918a1d298a3c593bdbe20a4d4e05f57
Sikai Zhao <sikaizhao1997@gmail.com>
2020-05-04 22:23:16 -0400

    Create Speciality Map Service Impl. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.petclinic.springpetclinic.services;
+
+import com.petclinic.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
