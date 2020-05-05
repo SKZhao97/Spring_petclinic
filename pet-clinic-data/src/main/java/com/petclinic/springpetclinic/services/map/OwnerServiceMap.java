9c08ba856ab19a455377b2515d85042b3f778362
Sikai Zhao <sikaizhao1997@gmail.com>
2020-05-04 22:15:25 -0400

    Enhance Owners with contact and Pets on Startup. Closes #33.

--- pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.petclinic.springpetclinic.services.map;
 
 import com.petclinic.springpetclinic.model.Owner;
+import com.petclinic.springpetclinic.model.Pet;
 import com.petclinic.springpetclinic.services.OwnerService;
+import com.petclinic.springpetclinic.services.PetService;
+import com.petclinic.springpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -30,7 +41,25 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if (pet.getId() != null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/petclinic/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/springpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.petclinic.springpetclinic.bootstrap;
 
 import com.petclinic.springpetclinic.model.Owner;
+import com.petclinic.springpetclinic.model.Pet;
 import com.petclinic.springpetclinic.model.PetType;
 import com.petclinic.springpetclinic.model.Vet;
 import com.petclinic.springpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.petclinic.springpetclinic.services.PetTypeService;
 import com.petclinic.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -37,12 +40,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("3213214321");
+
+        Pet fionasPet = new Pet();
+        fionasPet.setName("Just cat");
+        fionasPet.setOwner(owner2);
+        fionasPet.setBirthDate(LocalDate.now());
+        fionasPet.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
 
