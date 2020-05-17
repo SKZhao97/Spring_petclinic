7265177be19ab4a1f2e970cc4662ef5aa338b389
Sikai Zhao <sikaizhao1997@gmail.com>
2020-05-16 23:57:08 -0400

    Implement findOwner fuctionalities. Closes #54

--- pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/petclinic/springpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.petclinic.springpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -74,5 +75,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .findFirst()
                 .orElse(null);
         return res;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //TODO
+        return null;
     }
 }
