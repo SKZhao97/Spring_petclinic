b98c3dc170fa8ba4a9873c10427895f1b841383e
Sikai Zhao <sikaizhao1997@gmail.com>
2020-05-16 21:36:46 -0400

    Display Owner Record HTML. Closes #53

--- pet-clinic-web/src/test/java/com/petclinic/springpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/petclinic/springpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,10 +53,29 @@ class OwnerControllerTest {
                 .andExpect(model().attribute("owners", hasSize(2)));
     }
 
+
+    @Test
+    void listOwnerByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(get("owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
     @Test
     void findOwners() throws Exception{
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
